865ab8fccc17dcc4a9de983cc33d7f0a49b3dcdb
dir-FY25suzukimoeka <moeka.suzuki.a827@dirbato.co.jp>
2025-05-23 18:35:59 +0900

    02課題

---
+++ Sample/src/Sample/Test.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class Test {
+    // mainメソッドはプログラムの最初に動作します。
+    main2{
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
+    }
+
+}
---
+++ Sample/src/Sample/sample.java
@@ -0,0 +1,16 @@
+package Sample;
+
+public class sample {
+
+    public static void main(String[] args) {
+        String if = "何らかのデータ";
+        if = "別の何か";
+
+        System.out.println(if);
+    }
+   }
+    
+
+    
+
+
